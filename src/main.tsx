20ebe2c6b7a41be2b0e724e12149feaf15c2f874
ViktorTasiev <victortasev@gmail.com>
2025-04-04 13:04:43 -0700

    fonts installed

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+// Import Fontsource packages
+import '@fontsource/bebas-neue';
+import '@fontsource-variable/inter';
+import '@fontsource-variable/league-spartan';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
